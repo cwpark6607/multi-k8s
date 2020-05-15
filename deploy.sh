fc72e602f636eb16de883a2ab245377a42701fbd
parkcw <parkcw@kr.ibm.com>
2020-05-15 15:03:55 +0900

    fixed multi-server tag

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker build -t cwpark6607/multi-client:latest -t cwpark6607/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t cwpakr6607/multi-server:latest -t cwpark6607/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t cwpark6607/multi-server:latest -t cwpark6607/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t cwpark6607/multi-worker:latest -t cwpark6607/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push cwpark6607/multi-client:latest
 docker push cwpark6607/multi-server:latest
