ebb1cf4adbf6f2b93318eac9555143045822b71c
parkcw <parkcw@kr.ibm.com>
2020-05-15 14:32:26 +0900

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t cwpark6607/multi-client:latest -t cwpark6607/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t cwpakr6607/multi-server:latest -t cwpark6607/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t cwpark6607/multi-worker:latest -t cwpark6607/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push cwpark6607/multi-client:latest
+docker push cwpark6607/multi-server:latest
+docker push cwpark6607/multi-worker:latest
+docker push cwpark6607/multi-client:$SHA
+docker push cwpark6607/multi-server:$SHA
+docker push cwpark6607/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=cwpark6607/multi-client:$SHA
+kubectl set image deployments/server-deployment server=cwpark6607/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=cwpark6607/multi-worker:$SHA
